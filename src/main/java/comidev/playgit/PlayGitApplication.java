aa39a40fe6255723ff9fd3fafb460149423621c4
Omar Miranda <comidev.contacto@gmail.com>
2022-09-04 19:50:39 -0500

    Creando proyecto

---
+++ src/main/java/comidev/playgit/PlayGitApplication.java
@@ -0,0 +1,13 @@
+package comidev.playgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comidev/playgit/PlayGitApplicationTests.java
@@ -0,0 +1,13 @@
+package comidev.playgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
