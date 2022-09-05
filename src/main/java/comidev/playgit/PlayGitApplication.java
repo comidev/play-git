ec513eddd778f3cbbe44da44a6e047df6300df70
Omar Miranda <comidev.contacto@gmail.com>
2022-09-04 20:03:20 -0500

    feature: creando get all customers

--- src/main/java/comidev/playgit/PlayGitApplication.java
+++ src/main/java/comidev/playgit/PlayGitApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlayGitApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PlayGitApplication.class, args);
 	}
-
 }
